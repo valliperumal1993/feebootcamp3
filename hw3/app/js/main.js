ed85b23199bd9ae4bcd0ae4669ef30e0c0f951a6
Srujana Siripurapu <siripurs@advisory.com>
2016-02-10 17:49:29 -0600

    Code Review Comments

--- hw3/app/js/main.js
+++ hw3/app/js/main.js
@@ -1,13 +1,19 @@
 'use strict';
 $(document).ready(function(){
-	$('#but1').on('click',function(){$.ajax({
-url:'http://www.google.com/finance/info?q=AAPL',
-type:"get",
-success:function(data)
-{console.log(data);}
-	})})
+	$('#but1').on('click',function(){
+//Use spaces for indentation of code blocks to improve code readability
+		$.ajax({
+			url:'http://www.google.com/finance/info?q=AAPL',
+			type:"get",
+			success:function(data)
+			{
+//Avoid leaving back console.log statements in production code
+				console.log(data);
+			}
+		})
+	})
 
-
+//Use single "var" declaration per scope as var accepts multiple declarations
 var source   = $("#entry-template").text();
 var template = Handlebars.compile(source);
 var context = { body: "gt"};
